ad2ca48ff5accf12ad71584e91125d1d86d2442e
effyOsvin <37514488+effyOsvin@users.noreply.github.com>
2018-04-04 19:50:20 +0300

    Update Generate.py

--- Generate.py
+++ Generate.py
@@ -1,6 +1,8 @@
 import re
-import json
+import argparse
 import numpy as np
+import random
+import pickle
 
 class Model:
     
@@ -17,18 +19,13 @@ class Model:
         self.__wnc = '_total_word_num'
         
     def load(self, file_name):
-        
-        
-        # используй pickle
-        
-        
         """
         Загружает массив словарей из текста в data.
         :file_name: имя файла, из которого мы берем информацию.
         :return: nothing
         """
-        with open(file_name) as file1:
-            buffer = json.load(file1)
+        with open(file_name, 'rb') as file1:
+            buffer = pickle.load(file1)
         self.data = {}
         for key in buffer.keys():
             self.data[tuple(key.split(' '))] = buffer[key]
@@ -38,20 +35,20 @@ class Model:
         """
         Для каждого значения для n-граммы считает вероятность вхождения слова.
         (нормирует частоты)
-        :b: значение __wnc для каждой n-граммы.  # давай не будем использовать однобуквенные переменные
+        :b: значение __wnc для каждой n-граммы.
         :predata: массив, в котором хранятся слова и вероятность их вхождения для каждой n-граммы.
+                    в одной куче, потому что это удобно для np.random.choice
         :return: nothing
         """
         self.predata = {}
-        for ngr in self.data.keys(): # for ngramm, ngramm_value in self.data.items():
-            self.predata[ngr] = []
-            self.predata[ngr].append(list(self.data[ngr].keys()))
-            b = self.data[ngr][self.__wnc]
-            self.data[ngr][self.__wnc] = 0  # зачем
-            self.predata[ngr].append(np.array(list(self.data[ngr].values())))  # в одном массиве лежат и вероятности и слова вперемешку?
-            self.predata[ngr][1] = self.predata[ngr][1] / np.int(b)  # почему [1]?!
-            
-            # вот этот кусочек (self._preprocess) надо прям переписать нормально
+        for ngramm, ngramm_value in self.data.items():
+            self.predata[ngramm] = []
+            self.predata[ngramm].append(list(ngramm_value.keys()))#добовляем список слов
+            summa = ngramm_value[self.__wnc]# запоминаем общую сумму для н-граммы
+            ngramm_value[self.__wnc] = 0# обнуляем общую сумму, чтобы она не мешала рассчету вероятностей
+            self.predata[ngramm].append(np.array(list(ngramm_value.values())))#добовляем список количества вхождений
+            self.predata[ngramm][1] = self.predata[ngramm][1] / np.int(summa) #в 1 лежали количества вхождений слова
+            # и мы их поделили на общую сумму вхождений слов. Получили список вероятностей
         
     
     def _get_random_after(self, ngr):
@@ -71,37 +68,44 @@ class Model:
         :cur_ngram: n-грамма, по которой ищем следующее слово.
         :return: последовательность заданной длины. 
         """
-        st = word  # названия переменных!
+        string = word
         cur_ngram = [word]
-        self.preprocess()  # мне кажется, что метод у тебя назывался немного по-другому
-        # значит код ты не запускала
-        # обидно
-        
+        self._preprocess()
         for i in range(1, text_len):
             word = self._get_random_after(cur_ngram)
-            st += " " + word
+            string += " " + word
             if len(cur_ngram) < self.n - 1:
                 cur_ngram.append(word)
             else:
                 cur_ngram = cur_ngram[1:]
                 cur_ngram.append(word)
             
-        return st
-
-n = 1
-m = Model(n)
+        return string
     
-parser = argparse.ArgumentParser()
-parser.add_argument("--model", help="path to the file from which the model is loaded")
-parser.add_argument("--seed", help="The initial word")  # а если слово не дали, надо уметь выбирать случайное из модели
-parser.add_argument("--length", type=int, help="length of the generated sequence") # дефолтную проставь
-parser.add_argument("--output", help="The file to which the result will be written")
+    def getRandomWord(self):
+        return random.choice(data.values().keys)
 
-args = parser.parse_args()
+    
+if __name__ == "__main__":
+    n = 1
+    m = Model(n)
 
-m.load(args.output)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--model", help="path to the file from which the model is loaded")
+    parser.add_argument("--seed", help="The initial word")
+    parser.add_argument("--length", type=int, default=1 ,help="length of the generated sequence")
+    parser.add_argument("--output", help="The file to which the result will be written")
 
-if args.output:
-    open(file_name, 'w').write(json.dumps(getText(args.seed), args.length)) 
-else:
-    print(getText(args.seed), args.length)
+    args = parser.parse_args()
+
+    m.load(args.output)
+
+    if args.seed:
+        word = getText(args.seed)
+    else:
+        word = m.getRandomWord
+
+    if args.output:
+        open(file_name, 'w').write(json.dumps(word, args.length)) 
+    else:
+        print(word, args.length)
